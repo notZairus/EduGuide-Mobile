be7d734a3213e882c2e45626f2a8115ce344b24f
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2026-01-20 21:24:45 +0800

    feat: app icon and app splash screen

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import "../global.css";
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-400">index</Text>
+    </View>
+  );
+};
+
+export default index;
